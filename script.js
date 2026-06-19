8ff600d7a5133c45243e0234843b38ddef17a6ef
csif <82277214+csifpro@users.noreply.github.com>
2026-06-19 16:59:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let level = 0; // починаємо з h1
+
+function addText() {
+    const title = document.getElementById("output");
+    if (level < 5) {
+        level++;
+        title.outerHTML = `<h${level} id="output">Heders(h${level})</h${level}>`;
+    }   else{
+            title.outerHTML = `<p id="end">Кінець</p>`;
+            document.body.style.backgroundImage = "url('8aba5216fb8c41d1bf322622e0e95ed6.jpg')";
+            document.body.style.backgroundSize = "cover";
+            document.body.style.backgroundPosition = "center";
+    }
+
+}
